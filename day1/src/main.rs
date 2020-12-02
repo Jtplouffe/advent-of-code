b73ce26c9216b85c9c41811acdc35d8fee28189e
Jtplouffe <jtplouffe@gmail.com>
2020-12-02 16:59:33 -0500

    Optimized day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,14 +4,16 @@ use std::io::{BufReader, BufRead};
 fn main() {
     let file = File::open("assets/input").expect("Could not find file");
     let reader = BufReader::new(file);
-    let lines: Vec<String> = reader.lines()
-        .map(|l| l.expect("Could not parse line"))
+    let lines: Vec<i32> = reader.lines()
+        .map(|l| {
+            l.expect("Could not parse line").parse::<i32>().expect("Could not convert to i32")
+        })
         .collect();
 
     for i in 0..lines.len() {
         for n in i + 1..lines.len() {
-            let a = lines.get(i).unwrap().parse::<i32>().expect("Could not convert to i32");
-            let b = lines.get(n).unwrap().parse::<i32>().expect("Could not convert to i32");
+            let a = lines.get(i).unwrap();
+            let b = lines.get(n).unwrap();
 
             if a + b == 2020 {
                 println!("Part 1: {}", a * b);
@@ -22,9 +24,9 @@ fn main() {
     for i in 0..lines.len() {
         for n in i + 1..lines.len() {
             for m in n + 1..lines.len() {
-                let a = lines.get(i).unwrap().parse::<i32>().expect("Could not convert to i32");
-                let b = lines.get(n).unwrap().parse::<i32>().expect("Could not convert to i32");
-                let c = lines.get(m).unwrap().parse::<i32>().expect("Could not convert to i32");
+                let a = lines.get(i).unwrap();
+                let b = lines.get(n).unwrap();
+                let c = lines.get(m).unwrap();
 
                 if a + b + c == 2020 {
                     println!("Part 2: {}", a * b * c);
