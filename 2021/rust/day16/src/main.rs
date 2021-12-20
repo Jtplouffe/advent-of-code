096d985eb43818eae34c4c5778fb1ab0750ca888
Jtplouffe <jtplouffe@gmail.com>
2021-12-19 21:40:15 -0500

    Cleanup

--- 2021/rust/day16/src/main.rs
+++ 2021/rust/day16/src/main.rs
@@ -1,4 +1,3 @@
-use std::time::Instant;
 use crate::packet::Packet;
 use crate::packet_value::PacketValue;
 
@@ -11,10 +10,8 @@ mod packet_length;
 mod packet_value;
 
 fn main() {
-    let i = Instant::now();
     let input = include_str!("input");
     let root_packet = Packet::from(input);
     println!("Part one: {}", root_packet.version_sum());
     println!("Part two: {}", root_packet.value());
-    println!("{:?}", i.elapsed());
 }
