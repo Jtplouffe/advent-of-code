c7503d15daaace15238e194cb308b4c1ec3d6649
jt <32107801+Jtplouffe@users.noreply.github.com>
2023-12-07 22:24:20 -0500

    Started

--- 2023/day7/src/hand.rs
+++ 2023/day7/src/hand.rs
@@ -6,8 +6,28 @@ use crate::{card::Card, hand_type::HandType};
 pub struct Hand {
     cards: Vec<Card>,
     hand_type: HandType,
+    hand_type_with_jokers: HandType,
 
     pub bid: usize,
+}
+
+impl Hand {
+    pub fn cmp_with_jokers(&self, other: &Self) -> Ordering {
+        match self.hand_type_with_jokers.cmp(&other.hand_type_with_jokers) {
+            Ordering::Equal => self
+                .cards
+                .iter()
+                .zip(other.cards.iter())
+                .find_map(
+                    |(card_left, card_right)| match card_left.cmp_with_jokers(&card_right) {
+                        Ordering::Equal => None,
+                        ordering => Some(ordering),
+                    },
+                )
+                .unwrap_or(Ordering::Equal),
+            ordering => ordering,
+        }
+    }
 }
 
 impl From<&str> for Hand {
@@ -16,12 +36,14 @@ impl From<&str> for Hand {
 
         let cards: Vec<_> = cards.chars().map(Card::from).collect();
         let hand_type = HandType::from_hand_cards(&cards);
+        let hand_type_with_jokers = HandType::from_hard_cards_with_jokers(&cards);
 
         let bid = bid.parse().expect("expected bid number");
 
         Self {
             cards,
             hand_type,
+            hand_type_with_jokers,
             bid,
         }
     }
